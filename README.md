41d25ca79dd243972cf323b0d4b4b1d19c8e5e0a
yamashin0922 <y@zenryokukikai.com>
2021-12-17 14:09:24 +0900

    The first commit for anagrammer.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # anagrammer
 A program that anagrams the keywords and outputs probable name candidates
+
+## Functions
+
+キーワードをカタカナで引数に指定すると、欧羅巴人名録（https://www.worldsys.org/europe/）を走査して、  
+キーワードをアナグラムすることで得られる実在する氏名の候補を出力する  
+
+
+## Usage
+
+python anagrammer.py メリーゴーランド  
+
+
+
+## Output
+
+https://www.worldsys.org/europe/dutch-female-names-0002/ Lienリーン  
+https://www.worldsys.org/europe/finnish-male-names-0002/ Lariラリ  
+https://www.worldsys.org/europe/dutch-male-names-0003/ Rienリーン  
+https://www.worldsys.org/europe/english-family-names-0014/ Leanリーン  
+https://www.worldsys.org/europe/italian-family-names-0018/ Melandriメランドリ  
+https://www.worldsys.org/europe/spanish-family-names-0007/ Liraリラ  
---
+++ anagrammer.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup as BS
+import re
+import sys
+import urllib
+from tqdm import tqdm
+
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+def getName(url):
+    names = []
+    usock = urllib.request.urlopen(url)
+    data = usock.read()
+    usock.close()
+    soup = BS(data,features="html.parser")
+    table_list = soup.find('table', {'class':'table_list'})
+    if table_list: 
+        for name in table_list.text.split():
+            names.append(name)
+    return names
+
+
+def getLinks(base_url, urls):
+    print('**',base_url)
+    reqs = requests.get(base_url)
+    soup = BS(reqs.text, 'html.parser')
+
+    if soup.title == None:
+        return
+    
+    if '女性' not in soup.title.string and \
+       '男性' not in soup.title.string and \
+       '姓' not in soup.title.string and \
+       '欧羅巴人名録' != soup.title.string:
+        return 
+    
+    for link in soup.find_all('a'):
+        if '#index' in link.get('href'):
+            continue
+        if './' == link.get('href'):
+            continue
+        if '..' == link.get('href'):
+            continue
+        if 'http' in link.get('href'):
+            continue
+        if 'javascript:void(0)' in link.get('href'):
+            continue
+        if './search' in link.get('href'):
+            continue
+        url = urllib.parse.urljoin(base_url , link.get('href'))
+        if url not in urls:
+            #getName(url, src)
+            urls.append(url )
+            getLinks(url, urls)
+
+
+def search_anagram(url, src_name):
+    urls = []
+    getLinks(url, urls)
+
+    candidate_names = []
+    for url in tqdm(urls):
+        sorted_src_name = ''.join(sorted(src_name))
+        for name in getName(url):
+            katakana = re.compile(r'[\u30A1-\u30F4・ー]+')
+            for kana in katakana.findall(name):
+                sorted_kana = ''.join(sorted(kana))
+                if sorted_kana in sorted_src_name:
+                    candidate_names.append((url, name))
+    return candidate_names
+
+
+if __name__ == '__main__':
+    src_name = sys.argv[1]
+    print('src name',src_name)
+    url = 'https://www.worldsys.org/europe/'
+
+    candidate_names = search_anagram(url, src_name)
+    if len(candidate_names):
+        for url, candidate_name in candidate_names:
+            print(url, candidate_name)
+    else:
+        print('not found')
+
